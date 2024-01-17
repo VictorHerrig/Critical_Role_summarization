b31b949379f4c49d45fc0eea095470137896d297
VictorHerrig <github.mhms0@simplelogin.com>
2024-01-17 11:55:32 -0800

    Moving from using the CRD3 repo to the huggingface dataset

--- src/CRD3_summarization/loaders/__init__.py
+++
@@ -1,1 +0,0 @@
-from .CRD3Dataset import *
--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='CRD3_summarization',
     version='0.0.2',
-    packages=['CRD3_summarization', 'CRD3_summarization.models', 'CRD3_summarization.loaders', 'CRD3_summarization.modules'],
+    packages=['CRD3_summarization', 'CRD3_summarization.models', 'CRD3_summarization.datasets', 'CRD3_summarization.modules'],
     package_data={'CRD3_summarization.modules': ['src/modules/lib/*.so']},
 #    install_requires=[
 #        'numpy>=1.24',
