1968c6f3dfc6efd0832c1e48efc2b54077caf01a
VictorHerrig <github.mhms0@simplelogin.com>
2024-01-28 16:15:32 -0800

    changed staticmethod to classmethod where applicable, add bidirectional speaker model

--- src/CRD3_summarization/SASR.py
+++ src/CRD3_summarization/SASR.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from faster_whisper import WhisperModel
 from transformers import AutoModel
@@ -12,25 +13,47 @@ class SpeakerRecognitionDecoder(torch.nn.Module):
     def __init__(
             self,
             num_speakers: int,
+            architecture_type: str,
             input_size: int,
-            nhead: int = 8,
+            nhead: int = 4,
             dim_feedforward: int = 512,
             dropout: float = 0.1,
             device: str = None,
             num_layers: int = 2
     ):
         super().__init__()
-        decoder_layer = torch.nn.TransformerDecoderLayer(
-            d_model=input_size,
-            nhead=nhead,
-            dim_feedforward=dim_feedforward,
-            dropout=dropout,
-            device=device
-        )
-        self._decoder = torch.nn.TransformerDecoder(
-            decoder_layer=decoder_layer,
-            num_layers=num_layers
-        )
+        assert architecture_type in ['decoder', 'encoder'],\
+            ValueError('Expected architecture_type to be either "decoder" or "encoder"')
+
+        # Transformer block
+        if architecture_type == 'decoder':
+            # Unidirectional
+            decoder_layer = torch.nn.TransformerDecoderLayer(
+                d_model=input_size,
+                nhead=nhead,
+                dim_feedforward=dim_feedforward,
+                dropout=dropout,
+                device=device
+            )
+            self._decoder = torch.nn.TransformerDecoder(
+                decoder_layer=decoder_layer,
+                num_layers=num_layers
+            )
+        else:
+            # Bidirectional
+            encoder_layer = torch.nn.TransformerEncoderLayer(
+                d_model=input_size,
+                nhead=nhead,
+                dim_feedforward=dim_feedforward,
+                dropout=dropout,
+                device=device
+            )
+            self._decoder = torch.nn.TransformerEncoder(
+                encoder_layer=encoder_layer,
+                num_layers=num_layers
+            )
+
+        # Linear + smax for classification
         self._linear = torch.nn.Linear(
             in_features=input_size,
             out_features=num_speakers
@@ -49,17 +72,46 @@ class SpeakerRecognitionDecoder(torch.nn.Module):
         logits = self._linear(decoder_out)
         return self._smax(logits)
 
+    @classmethod
+    def encoder_model(
+            cls,
+            num_speakers: int,
+            input_size: int,
+            **kwargs
+    ):
+        return cls(
+            num_speakers=num_speakers,
+            architecture_type='encoder',
+            input_size=input_size,
+            **kwargs
+        )
+
+    @classmethod
+    def decoder_model(
+            cls,
+            num_speakers: int,
+            input_size: int,
+            **kwargs
+    ):
+        return cls(
+            num_speakers=num_speakers,
+            architecture_type='decoder',
+            input_size=input_size,
+            **kwargs
+        )
+
 
 class SAASRModel(torch.nn.Module):
     def __init__(
             self,
             asr_model_size_or_path: str,
-            speaker_model_path: str,
+            speaker_model_path: str = None,
             use_gpu_for_asr: bool = False,
-            user_gpu_for_speaker: bool = False,
+            use_gpu_for_speaker: bool = False,
             compute_type: str = 'default',
             cpu_threads: int = 0,
-            num_workers: int = 1
+            num_workers: int = 1,
+            speaker_model_kwargs: dict = None
     ) -> None:
         super().__init__()
         self._whipser_model = WhisperModel(
@@ -69,9 +121,17 @@ class SAASRModel(torch.nn.Module):
             cpu_threads=cpu_threads,
             num_workers=num_workers
         )
-        self._speaker_recognition_model: SpeakerRecognitionDecoder = AutoModel.from_pretrained(
-            speaker_model_path
-        )
+        if speaker_model_path is not None:
+            # Load a pretrained speaker recognition model
+            self._speaker_recognition_model: SpeakerRecognitionDecoder = AutoModel.from_pretrained(
+                speaker_model_path,
+                device_map='auto' if use_gpu_for_speaker else 'cpu'  # TODO: Make this better
+            )
+        else:
+            # Create a new speaker recognition model for training
+            assert speaker_model_kwargs is not None,\
+                ValueError('One of speaker_model_kwargs or speaker_model_path must be passed')
+            self._speaker_recognition_model = SpeakerRecognitionDecoder(**speaker_model_kwargs)
 
     def forward(
             self,
@@ -90,4 +150,4 @@ class SAASRModel(torch.nn.Module):
             *args,
             **kwargs
     ) -> ...:
-        return 1
+        raise NotImplementedError()
